9360952abffb099add0dd5b078a8feb0d6c67aa2
Universenn <dnwndhks98@gmail.com>
2022-11-10 11:25:10 +0900

    first commit

---
+++ src/test/java/com/mustache/bbs1110/SpringbootMustacheBbs1110ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustache.bbs1110;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMustacheBbs1110ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
